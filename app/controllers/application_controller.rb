58a614bca2f969a5ed0437eefe9f795eec9de691
キム アヨン <kimahyoung@team-lab.com>
2019-01-16 18:06:26 +0900

    多言語対応の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
+  # 多言語対応のため追加
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = locale
+  end
+
+  def locale
+    @locale ||= params[:locale] || I18n.default_locale
+  end
+
   include Response
   include ExceptionHandler
 end
--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,9 @@ module ProductsApi
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # 多言語対応のため追加
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
